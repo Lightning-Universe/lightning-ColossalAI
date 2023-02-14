60c397bcc73f421ad2723d4fc0195d570966916d
Adrian Wälchli <aedu.waelchli@gmail.com>
2023-02-14 17:42:04 -0500

    Initial commit

---
+++ src/pl_sandbox/__about__.py
@@ -0,0 +1,29 @@
+__version__ = "0.0.0dev"
+__author__ = "Lightning-AI et al."
+__author_email__ = "name@lightning.ai"
+__license__ = "TBD"  # TODO
+__copyright__ = f"Copyright (c) 2020-2022, {__author__}."
+__homepage__ = "https://github.com/Lightning-AI/lightning-sandbox"
+__docs__ = "PyTorch Lightning Sample project."
+# todo: consider loading Readme here...
+__long_doc__ = """
+What is it?
+-----------
+This is starter project template which shall simplify initial steps for each new PL project...
+
+Except the implemented sections:
+ - sample package
+ - setting CI
+ - setting Docs
+"""
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__copyright__",
+    "__docs__",
+    "__long_doc__",
+    "__homepage__",
+    "__license__",
+    "__version__",
+]
