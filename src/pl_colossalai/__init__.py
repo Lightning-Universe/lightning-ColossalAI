94e5c305271baa4fd6c3c52ee3c1acb651adbea7
Jirka <jirka.borovec@seznam.cz>
2023-02-15 04:30:08 +0100

    init fix

--- src/pl_colossalai/__init__.py
+++ src/pl_colossalai/__init__.py
@@ -1,8 +1,11 @@
-"""Root package info."""
-
 import os
 
 from pl_colossalai.__about__ import *  # noqa: F401, F403
+from pl_colossalai.precision import ColossalAIPrecisionPlugin
+from pl_colossalai.strategy import ColossalAIStrategy
 
 _PACKAGE_ROOT = os.path.dirname(__file__)
 _PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
+
+
+__all__ = ["ColossalAIStrategy", "ColossalAIPrecisionPlugin"]
